5098686a92cd9ee50d7b6254684380bf6de74851
Martin Griffin <martinrgriffin@gmail.com>
2026-04-23 19:50:30 +0100

    fixup! Remove SaveBlock/PokemonStorage ASLR

--- src/overworld.c
+++ src/overworld.c
@@ -130,7 +130,7 @@ static void InitObjectEventsLocal(void);
 static void InitOverworldGraphicsRegisters(void);
 static u8 GetSpriteForLinkedPlayer(u8);
 static u16 KeyInterCB_SendNothing(u32);
-static void ResetMirageTowerAndSaveBlockPtrs(void);
+static void ResetMirageTowerAndHeap(void);
 static void ResetScreenForMapLoad(void);
 static void OffsetCameraFocusByLinkPlayerId(void);
 static void SpawnLinkPlayers(void);
@@ -2179,7 +2179,7 @@ static bool32 LoadMapInStepsLink(u8 *state)
         InitOverworldBgs();
         ScriptContext_Init();
         UnlockPlayerFieldControls();
-        ResetMirageTowerAndSaveBlockPtrs();
+        ResetMirageTowerAndHeap();
         ResetScreenForMapLoad();
         (*state)++;
         break;
@@ -2261,7 +2261,7 @@ static bool32 LoadMapInStepsLocal(u8 *state, bool32 a2)
         (*state)++;
         break;
     case 1:
-        ResetMirageTowerAndSaveBlockPtrs();
+        ResetMirageTowerAndHeap();
         ResetScreenForMapLoad();
         (*state)++;
         break;
@@ -2333,7 +2333,7 @@ static bool32 ReturnToFieldLocal(u8 *state)
     switch (*state)
     {
     case 0:
-        ResetMirageTowerAndSaveBlockPtrs();
+        ResetMirageTowerAndHeap();
         ResetScreenForMapLoad();
         ResumeMap(FALSE);
         InitObjectEventsReturnToField();
@@ -2368,7 +2368,7 @@ static bool32 ReturnToFieldLink(u8 *state)
     {
     case 0:
         FieldClearVBlankHBlankCallbacks();
-        ResetMirageTowerAndSaveBlockPtrs();
+        ResetMirageTowerAndHeap();
         ResetScreenForMapLoad();
         (*state)++;
         break;
@@ -2444,7 +2444,7 @@ static void DoMapLoadLoop(u8 *state)
     while (!LoadMapInStepsLocal(state, FALSE));
 }
 
-static void ResetMirageTowerAndSaveBlockPtrs(void)
+static void ResetMirageTowerAndHeap(void)
 {
     ClearMirageTowerPulseBlend();
     RotateEncryptionKey_ResetHeap();
